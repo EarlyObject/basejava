ccdb81e4af79b01f730ff7dbbe0075fc3adf0b5e
EarlyObject <sr.ardak@gmail.com>
2020-01-31 16:42:17 +0100

    HW5. ListStorageTest class implemented MapStorageTest class implemented

---
+++ test/ru/javawebinar/basejava/storage/MapStorageTest.java
@@ -0,0 +1,93 @@
+package ru.javawebinar.basejava.storage;
+
+import org.junit.Before;
+import org.junit.Test;
+import ru.javawebinar.basejava.exception.NotExistStorageException;
+import ru.javawebinar.basejava.model.Resume;
+
+import static org.junit.Assert.*;
+
+public class MapStorageTest {
+    private Storage storage;
+
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
+    private static final String UUID_4 = "uuid4";
+
+    private static final Resume RESUME_1 = new Resume(UUID_1);
+    private static final Resume RESUME_2 = new Resume(UUID_2);
+    private static final Resume RESUME_3 = new Resume(UUID_3);
+    private static final Resume RESUME_4 = new Resume(UUID_4);
+
+    public MapStorageTest() {
+        storage = new SortedArrayStorage();
+    }
+
+    @Before
+    public void setUp() {
+        storage.clear();
+        storage.save(RESUME_1);
+        storage.save(RESUME_2);
+        storage.save(RESUME_3);
+    }
+
+    @Test
+    public void save() {
+        storage.save(RESUME_4);
+        assertEquals(4, storage.size());
+        assertEquals(RESUME_4, storage.get(UUID_4));
+    }
+
+    @Test
+    public void get() {
+        assertEquals(RESUME_2, storage.get(UUID_2));
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void getNotExist() {
+        storage.get("dummy");
+    }
+
+    @Test
+    public void update() {
+        Resume testResume = new Resume(UUID_2);
+        storage.update(testResume);
+        assertSame(testResume, storage.get(UUID_2));
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void updateNotExist() {
+        storage.update(RESUME_4);
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void delete() {
+        storage.delete(UUID_1);
+        assertEquals(2, storage.size());
+        storage.get(UUID_1);
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void deleteNotExist() {
+        storage.delete(UUID_4);
+    }
+
+    @Test
+    public void size() {
+        assertEquals(3, storage.size());
+    }
+
+    @Test
+    public void clear() {
+        storage.clear();
+        assertEquals(0, storage.size());
+    }
+
+    @Test
+    public void getAll() {
+        Resume[] test2 = {RESUME_1, RESUME_2, RESUME_3};
+        assertArrayEquals(test2, storage.getAll());
+        assertEquals(3, storage.getAll().length);
+    }
+}
